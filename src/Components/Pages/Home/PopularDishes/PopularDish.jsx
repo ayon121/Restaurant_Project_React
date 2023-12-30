f90ee2c3834bb19f37b9ee3080fc9567b8387619
ayon121 <borshonsaha1234@gmail.com>
2023-12-30 21:57:04 +0600

    marquee added

--- src/Components/Pages/Home/PopularDishes/PopularDish.jsx
+++ src/Components/Pages/Home/PopularDishes/PopularDish.jsx
@@ -3,7 +3,7 @@
 
 const PopularDish = () => {
     return (
-        <div className="card w-auto bg-base-100 shadow-xl image-full">
+        <div className="card w-4/5 md:w-4/5 lg:w-4/5  bg-base-100 shadow-xl image-full">
             <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">Pizza</h2>
--- src/Components/Pages/Home/PopularDishes/PopularDishes.jsx
+++ src/Components/Pages/Home/PopularDishes/PopularDishes.jsx
@@ -1,17 +1,21 @@
 import Title from "../../../Shared/Title";
 import PopularDish from "./PopularDish";
-
+import Marquee from "react-fast-marquee";
 
 const PopularDishes = () => {
     return (
         <div>
             <Title title={'Popular Dishes'}></Title>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 px-4 ">
-                <PopularDish></PopularDish>
-                <PopularDish></PopularDish>
-                <PopularDish></PopularDish>
-            </div>
-            
+            <Marquee autoFill="true" >
+                <div className="grid grid-cols-3  gap-1 px-2 max-w-7xl mx-auto mb-5">
+
+                    <PopularDish></PopularDish>
+                    <PopularDish></PopularDish>
+                    <PopularDish></PopularDish>
+
+                </div>
+            </Marquee>
+
         </div>
     );
 };
