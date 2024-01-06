3ff7dfa6536da0829529b890baef00dc82699162
ayon121 <borshonsaha1234@gmail.com>
2024-01-06 15:38:27 +0600

    banner & story design 2nd update

--- src/Components/Pages/Home/OurStory/OurStory.jsx
+++ src/Components/Pages/Home/OurStory/OurStory.jsx
@@ -8,7 +8,7 @@ const OurStory = () => {
             <div className="flex flex-col md:flex-row justify-center items-start gap-3 px-5 mb-2 max-w-5xl mx-auto ">
                 
                 {/* story */}
-                <div className="grow w-3/4">
+                <div className="grow w-full md:w-3/4">
                 <h1 className="text-2xl md:text-4xl font-bold mb-3"> The Extraordinary Journey of Culina</h1>
                     <p className="text-xs  md:text-sm lg:text-lg w-4/5">Welcome to Culina, where every dish tells a story steeped in passion, dedication, and a love for culinary artistry. Our journey began with a dream to create a haven where flavors come alive and moments turn into memories.</p>
                     <p className="text-xs  md:text-sm lg:text-lg w-4/5">
@@ -20,9 +20,9 @@ const OurStory = () => {
                     <a className="btn btn-sm md:btn-md font-extrabold bg-highlight_color text-main  hover:bg-sub_color_1 hover:text-highlight_color border-highlight_color my-4">Know More</a>
                 </div>
                 {/* img */}
-                <div className=" md:w-1/3  h-screen ">
+                <div className=" md:w-1/3 h-4/5 ">
                     <div>
-                        <img className="h-3/4 rounded-2xl" src={storypic} alt="our story" />
+                        <img className="  hidden md:block md:h-3/4 rounded-2xl" src={storypic} alt="our story" />
                     </div>
                 </div>
             </div>
--- src/Components/Pages/Home/PopularDishes/PopularDish.jsx
+++ src/Components/Pages/Home/PopularDishes/PopularDish.jsx
@@ -5,12 +5,12 @@ import popular from '../../../../assets/popularbg.jpg'
 
 const PopularDish = ({title}) => {
     return (
-        <div className="card w-4/5 md:w-4/5 lg:w-4/5  bg-base-100 shadow-xl image-full border-2 border-highlight_color font-Nunito">
-            <figure><img src={popular} alt="popular img" /></figure>
-            <div className="card-body">
+        <div className="card w-4/5 md:w-4/5 lg:w-4/5  bg-base-100 shadow-xl image-full border-2 border-highlight_color rounded-2xl font-Nunito">
+            <figure><img  src={popular} alt="popular img" /></figure>
+            <div className="card-body rounded-3xl">
                 <h2 className="text-2xl md:text-4xl text-center ">{title}</h2>
                 <hr />
-                <p className='text-center'>Savor the magic of quick bites that dance on your taste buds, delivering a culinary experience thats as swift as it is sensational. Elevate your cravings with our delectable offerings, where every bite is a celebration of taste, speed, and pure delight.</p>
+                <p className='text-center'>Savor the magic of quick bites that dance on your taste buds, delivering a culinary experience thats as swift as it is sensational.</p>
             </div>
         </div>
     );
